8260c3b781ee5fd265e5635ead891413d8644791
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-01-18 03:12:30 +0700

    complete dashboard

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
+import './assets/css/grid.css';
+import './assets/css/index.css';
+import Layout from './components/layout/Layout';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Layout />
     </React.StrictMode>,
     document.getElementById('root')
 );
